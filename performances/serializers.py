cc01357f559a453bdef095b925b1b265e35f4125
jack <you@example.com>
2021-04-10 14:22:18 +0300

    Add an head of institution field.

--- performances/serializers.py
+++ performances/serializers.py
@@ -3,11 +3,13 @@ from .models import Institution
 
 
 class InstitutionSerializer(serializers.ModelSerializer):
+    head_of_institution = serializers.ReadOnlyField(source='head_of_institution.__str__')
 
     class Meta:
         model = Institution
         fields = (
             'name',
+            'head_of_institution',
             'institution_type',
             'zone',
             'sub_county',
