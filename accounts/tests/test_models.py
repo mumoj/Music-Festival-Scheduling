8481643dadac32f47851f9f3f7411a891e6330d8
jack <you@example.com>
2021-04-18 17:20:02 +0300

    Test the added get_long_name() method.

--- accounts/tests/test_models.py
+++ accounts/tests/test_models.py
@@ -16,11 +16,14 @@ class CustomUserModelTestClass(TestCase):
     def setUp(self) -> None:
         self.custom_user = CustomUser.objects.get(email='normal@gmail.com')
 
-    def test__str__(self):
-        self.assertEqual(self.custom_user.__str__(), 'Naby Deco Keita')
+    def test_long_name(self):
+        self.assertEqual(self.custom_user.get_long_name(), 'Naby Deco Keita')
 
     def test_get_short_name(self):
         self.assertEqual(self.custom_user.get_short_name(), 'Naby')
+
+    def test__str__(self):
+        self.assertEqual(self.custom_user.__str__(), 'normal@gmail.com')
 
 
 
