063cfea9a9d09759129a0d1ece233c10ae86da54
jack <you@example.com>
2021-04-24 11:01:48 +0300

    Complete test fixture for accounts.views.

--- accounts/tests/test_views.py
+++ accounts/tests/test_views.py
@@ -1,39 +1,17 @@
-from pprint import pprint
 from model_bakery import baker
-from rest_framework import status
-from rest_framework.test import APITestCase, APIClient
+from rest_framework.test import APITestCase
+from pprint import pprint
 
 from django.urls import reverse
-from accounts.permission_groups import HEADS_OF_INSTITUTION_GROUP
+
+from accounts.permission_groups import set_heads_of_institutions_group
 from accounts.models import (
     SponsorProfile,
     DependentPerformerProfile,
     CustomUser)
+
 from performances.models import Institution
-from django.contrib.auth.models import Group
 
-
-# class DependentPerformerRegistrationAndProfileViewTests(APITestCase):
-#     """
-#     Test how dependent performer registration is handled by the system.
-#     """
-#     def test_dependent_performer_registration(self):
-#         """
-#         Test creation of the dependent performer profile.
-#         """
-#         url = reverse('accounts:register')
-#         data = {
-#             'first_name': 'John',
-#             'middle_name': 'Mlachake',
-#             'last_name': 'Doe',
-#             'email': 'mlachake@gmail.com',
-#             'role': 'DEPENDENT_PERFORMER',
-#             'password1': '1234',
-#             'password2': '1234',
-#         }
-#         self.client.post(url, data, format='json')
-#         self.assertIsInstance(DependentPerformerProfile.objects.get(
-#             user__email=data['email']), DependentPerformerProfile)
 
 class HeadOfInstitutionRegistrationTest(APITestCase):
     """
@@ -43,7 +21,7 @@ class HeadOfInstitutionRegistrationTest(APITestCase):
     def setUp(self) -> None:
         """"""
         self.registration_url = reverse('accounts:register')
-        self.data = {
+        self.user_data = {
             'first_name': 'John',
             'middle_name': 'Mlachake',
             'last_name': 'Doe',
@@ -52,9 +30,58 @@ class HeadOfInstitutionRegistrationTest(APITestCase):
             'password1': '1234',
             'password2': '1234',
         }
+        self.head_of_institution_group = set_heads_of_institutions_group()
+        self.client.post(self.registration_url, self.user_data, format='json')
+        self.institution = Institution.objects.get(
+            head_of_institution__email=self.user_data['email'])
 
     def test_creation_of_an_institution_instance_on_registration(self):
-         pass
+        self.assertIsInstance(self.institution, Institution)
+
+
+class DependentPerformerRegistrationAndProfileViewTests(APITestCase):
+    """
+    Test how sponsor registration is handled by the system.
+    """
+
+    def setUp(self) -> None:
+        self.registration_url = reverse('accounts:register')
+        self.data = {
+            'first_name': 'John',
+            'middle_name': 'Mlachake',
+            'last_name': 'Doe',
+            'email': 'mlachake@gmail.com',
+            'role': 'DEPENDENT_PERFORMER',
+            'password1': '1234',
+            'password2': '1234',
+        }
+        self.client.post(self.registration_url, self.data, format='json')
+        self.dependent_performer_profile = DependentPerformerProfile.objects.get(
+            user__email=self.data['email'])
+
+    def test_a_dependent_performer_profile_instance_is_created_on_registration(self):
+        self.assertIsInstance(self.dependent_performer_profile, DependentPerformerProfile)
+
+    def test_add_dependent_profile_view(self):
+        """
+        Test sponsors are authenticated and
+        they and they only are able to add their own profile instances.
+        """
+        self.user = CustomUser.objects.get(email=self.data['email'])
+        self.client.login(email=self.user.email, password=1234)
+        self.performance = baker.make('performances.Performance')
+        self.institution = baker.make('performances.Institution')
+
+        update_url = reverse(
+            'accounts:update-dependent_performer',
+            kwargs={'dependent_performer': self.dependent_performer_profile.pk})
+
+        response = self.client.put(
+            update_url,
+            {'institution': self.institution.pk, 'performance': [self.performance.pk]},
+            format='json')
+        pprint(response.__dict__)
+        self.assertEqual(response.status_code, 200)
 
 
 class SponsorRegistrationAndProfileViewTests(APITestCase):
@@ -77,15 +104,21 @@ class SponsorRegistrationAndProfileViewTests(APITestCase):
 
     def test_sponsor_profile_instance_is_created_on_registration(self):
         self.assertIsInstance(SponsorProfile.objects.get(
-            user__email=self.data['email']), SponsorProfile)
+            user__email=self.data['email']), SponsorProfile
+        )
 
     def test_add_sponsor_profile_view(self):
         """
-        Test sponsors are authenticated and are adding their own profile instances.
+        Test sponsors are authenticated and that they are adding their own profile instances.
         """
         self.user = CustomUser.objects.get(email=self.data['email'])
         self.client.login(email=self.user.email, password=self.data['password1'])
 
-        update_url = reverse('accounts:update-sponsor', kwargs={'sponsor': self.user.pk})
-        response = self.client.put(update_url, {"payment_method": "PAYPAL"}, format='json')
+        update_url = reverse(
+            'accounts:update-sponsor',
+            kwargs={'sponsor': self.user.pk})
+        response = self.client.put(
+            update_url,
+            {"payment_method": "PAYPAL"},
+            format='json')
         self.assertEqual(response.status_code, 200)
