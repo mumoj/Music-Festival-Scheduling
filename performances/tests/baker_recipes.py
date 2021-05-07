26f03974d1658eb9000275db2dee6d564523604c
jack <you@example.com>
2021-05-07 17:40:37 +0300

    Add model_bakery recipes for performances app.

--- performances/tests/baker_recipes.py
+++ performances/tests/baker_recipes.py
@@ -1,13 +1,26 @@
 """ Prepare Recipes for the model_bakery  test model instances """
 from itertools import cycle
-from model_bakery.recipe import Recipe
-from performances.models import Class, Performance
+from model_bakery.recipe import Recipe, foreign_key
+from performances.models import (
+    Class, Performance,
+    Theater, Event)
 
 
 performance_class = Recipe(
     Class,
-    performance_duration=5
+    performance_duration=5)
+
+event = Recipe(
+    Event, venue="Bomas of Kenya")
+
+theater_names = ['Chapel', 'Mandela\'s Hall', 'Auditorium A', ]
+theater = Recipe(
+    Theater,
+    name=cycle(theater_names),
+    venue=foreign_key(event)
 )
+
+
 
 
 
