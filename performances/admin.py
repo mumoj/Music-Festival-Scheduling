410b0d0097b5492c3d8f5f9f7a34d79dfe00d0ef
jack <you@example.com>
2021-05-19 11:05:39 +0300

    Register Theater and Event models to admin.

--- performances/admin.py
+++ performances/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from .models import (Institution, Class,
-                     Performance)
+from .models import (
+    Institution, Class,
+    Performance, Theater,
+    Event)
 
 # Register your models here.
 admin.site.register(Institution)
 admin.site.register(Class)
 admin.site.register(Performance)
+admin.site.register(Theater)
+admin.site.register(Event)
