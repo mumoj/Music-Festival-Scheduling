0aa172155aeb4efbfad63b01ef1964c5d5f8c91a
jack <you@example.com>
2021-05-19 11:01:56 +0300

    Assembled performance scheduling module and its tests.

--- performances/performance_scheduling.py
+++ performances/performance_scheduling.py
@@ -1,5 +1,6 @@
 import datetime
 import asyncio
+from pprint import pprint
 from typing import Union, List
 
 from django.db.models import QuerySet
@@ -8,37 +9,154 @@ from django.db.models.query import EmptyQuerySet
 from performances.models import Class, Event
 
 
-def schedule_performances_for_each_theatre() -> dict:
-    all_classes = Class.objects.all().order_by('performance_duration')
-    total_time_taken = total_time_taken_by_all_classes(classes=all_classes)
-    theaters = Event.theater_set.all()
+def schedule_performances_for_each_theater(
+        event: QuerySet,
+        all_classes: Union[QuerySet, List[Class]]) -> dict:
+    """
+    Parameters
+    ----------
+    all_classes: All classes being performed in the event.
+    event: QuerySet
+        The festival event for which the timetable is being constructed.
 
-    festival_sessions: tuple = sessions_in_a_day()
+    Returns
+    -------
+        theaters_assigned_performances_per_day: dict
+             A dict with the distribution of performances among theaters and festival sessions
+            For example:
+                <Theater: Mandela's Hall>: {1: {datetime.datetime(1900, 1, 1, 8, 0):[
+                                                       <Performance: Performance object (48512)>,
+                                                       <Performance: Performance object (48511)>,
+                                                       <Performance: Performance object (48510)>,
+                                                       <Performance: Performance object (48509)>,
+                                                       <Performance: Performance object (48518)>,
+                                                       <Performance: Performance object (48517)>,
+                                                       <Performance: Performance object (48516)>,
+                                                       <Performance: Performance object (48515)>,
+                                                       <Performance: Performance object (48514)>],
+                                 datetime.datetime(1900, 1, 1, 11, 30): [],
+                                 datetime.datetime(1900, 1, 1, 14, 0): []}}}
+
+    """
+
+    total_time_taken = total_time_taken_by_all_classes(classes=all_classes)
+    theaters = event.theater_set.all()
+
     theaters_assigned_performances = divide_classes_among_theaters(
         total_time_taken=total_time_taken,
         classes=all_classes,
         theaters=theaters)
 
-    for key, value in theaters_assigned_performances.items():
-        performance_classes: list = theaters_assigned_performances[key]
-        theaters_assigned_performances[key] = {}  # Initialize a dict for sessions
-        theaters_assigned_performances_per_session: dict = theaters_assigned_performances
+    for theater, value in theaters_assigned_performances.items():
+        performance_classes: list = theaters_assigned_performances[theater]
+        theaters_assigned_performances[theater] = {}  # Initialize a dict for sessions in an event day.
+        theaters_assigned_performances_per_day: dict = theaters_assigned_performances
 
-        for session in festival_sessions:
-            session_dict = {session[1]: []}  # Initialize a  dict for a session performances.
-            theaters_assigned_performances[key].update(session_dict)
+        day = 1
+        left_over_performances: list = []
+        festival_sessions_in_a_day = list(sessions_in_a_day())
+        day_performances: dict = {day: {}}
+        theaters_assigned_performances_per_day[theater].update(day_performances)
+        session_performances: dict = {session[1]: [] for session in festival_sessions_in_a_day}
+        # Initialize an empty list for each session
+        theaters_assigned_performances_per_day[theater][day].update(session_performances)
+        for performance_class in performance_classes:
+            results: dict = event_day_scheduling(
+                left_over_performances=left_over_performances,
+                festival_sessions=festival_sessions_in_a_day,
+                performances_in_a_day_per_session={},
+                class_performances=list(performance_class.performance_set.all()))
 
-            performances_scheduled: tuple = session_performance_scheduling(
-                ordered_classes=performance_classes,
-                session_time=session[0])
+            for session_key, performances in results[0].items():  # Add performances to each session.
+                theaters_assigned_performances_per_day[theater][day][session_key] += performances
 
-            if performances_scheduled[0]:
-                theaters_assigned_performances[key][session[1]] = performances_scheduled[0]
+            for session in results[2]:  # Iterate over balance  sessions' time
+                if session[0] >= 5:
+                    festival_sessions_in_a_day = results[2]  # Use whatever balance session time remains to depletion.
+                    break
             else:
-                performances_scheduled = session_performance_scheduling(
-                    ordered_classes=performance_classes,
-                    session_time=session[0])
-        return theaters_assigned_performances_per_session
+                festival_sessions_in_a_day = list(sessions_in_a_day())
+                day += 1
+                day_performances: dict = {day: {}}
+                theaters_assigned_performances_per_day[theater].update(day_performances)
+                session_performances: dict = {session[1]: [] for session in festival_sessions_in_a_day}
+                theaters_assigned_performances_per_day[theater][day].update(session_performances)
+
+            left_over_performances = results[1]
+
+    return theaters_assigned_performances_per_day
+
+
+def event_day_scheduling(
+        left_over_performances: list,
+        festival_sessions: list,
+        performances_in_a_day_per_session: dict,
+        class_performances: list):
+    """
+    Parameters
+    ----------
+    class_performances: list
+        All the performances belonging to a particular class.
+    left_over_performances: list
+        Whatever class performances don't fit into a session.
+    festival_sessions: list
+        The defined sessions in a festival day.
+    performances_in_a_day_per_session: dict
+        Where performances are allocated into the various sessions in  single day.
+
+    Returns
+    -------
+    performances_in_a_day_per_session: dict
+
+    """
+    festival_sessions = [list(session_tuple) for session_tuple in festival_sessions]
+    for i, session in enumerate(festival_sessions):
+        session_dict = {session[1]: []}  # Initialize a  dict for a session's performances.
+        performances_in_a_day_per_session.update(session_dict)
+
+        time_taken_by_left_over_performances = 0
+        for performance in left_over_performances:  # Find the total time taken by left over performances.
+            time_taken_by_left_over_performances += performance.performance_class.performance_duration
+
+        if time_taken_by_left_over_performances < session[0]:
+            performances_in_a_day_per_session[session[1]] = left_over_performances
+            session[0] -= time_taken_by_left_over_performances
+
+            results: tuple = class_performances_scheduling(
+                class_performances=class_performances,
+                session_time=session[0],
+                scheduled_performances=[],
+                balance_performances=[])
+
+            if results[0]:  # If performances are scheduled to the session.
+                performances_in_a_day_per_session[session[1]] = results[0]
+                festival_sessions[i][0] = results[2]  # Update the session time.
+                class_performances = results[1]  # Update class_performances for next session.
+
+        elif time_taken_by_left_over_performances > session[0]:
+            # If leftover performances do not fit into session time.
+            results: tuple = class_performances_scheduling(
+                class_performances=left_over_performances,
+                session_time=session[0],
+                scheduled_performances=[],
+                balance_performances=left_over_performances)
+            scheduled_performances = results[0]
+            performances_in_a_day_per_session[session[1]] = \
+                performances_in_a_day_per_session[session[1]] + scheduled_performances
+            balance_performances = results[1]
+            left_over_performances = balance_performances
+            festival_sessions[i][0] = 0  # Record depletion of session time.
+            continue
+        else:
+            if time_taken_by_left_over_performances > session[0]:
+                performances_in_a_day_per_session[session[1]] = left_over_performances
+                festival_sessions[i][0] = 0  # Record depletion of session time.
+                continue
+
+    if class_performances:  # Set whatever performances remain to be scheduled the next day.
+        left_over_performances += class_performances
+
+    return performances_in_a_day_per_session, left_over_performances, festival_sessions
 
 
 def divide_classes_among_theaters(
@@ -46,101 +164,120 @@ def divide_classes_among_theaters(
         theaters: Union[QuerySet, List[Class]],
         classes: Union[QuerySet, List[Class]]) -> dict:
     """
-    Divide the classes among available theaters.
+    Divide the class es among available theaters.
+
+    Parameters
+    ----------
+    total_time_taken: int
+        Total time taken by the all performances in an event.
+    theaters: QuerySet
+        All the theaters available in an event.
+    classes:
+        All the classes allowed in an event.
+
+    Returns
+    -------
+    performance_classes_per_theater: dict
+        A dict containing dicts of all the theaters with their allocated theaters.
     """
+
     no_of_theaters: int = len(theaters)
     average_time_per_theater: float = total_time_taken / no_of_theaters
     performance_classes_per_theater: dict = {}
     remaining_classes: list = list(classes)
 
     for theater in theaters:
-        time_to_theater = 0
+        time_to_theater: int = 0  # Time allocated to theater.
         performance_classes_per_theater.update({theater: []})
 
-        for performance_class in remaining_classes:
-            time_taken = performance_class.performance_duration * \
-                         performance_class.performance_set.count()
+        remaining_classes[:] = [c for c in remaining_classes if c is not None]
+        for i, performance_class in enumerate(remaining_classes):
 
+            no_of_performances: int = performance_class.performance_set.count()
+            if no_of_performances == 0:  # Exclude classes with no registered performances.
+                remaining_classes[i] = None  # Remove class
+                continue
+
+            time_taken = performance_class.performance_duration * no_of_performances
             if time_to_theater < average_time_per_theater:
                 time_to_theater += time_taken
                 performance_classes_per_theater[theater].append(performance_class)
-                remaining_classes.pop(0)  # Remove a class once it is allocated to a theater.
+                remaining_classes[i] = None  # Remove a class once it is allocated to a theater.
             else:
-                break
+                if time_to_theater >= average_time_per_theater:
+                    break
     return performance_classes_per_theater
 
 
-def total_time_taken_by_all_classes(classes) -> int:
+def total_time_taken_by_all_classes(classes: Union[QuerySet, List[Class]]) -> int:
     """
-    Calculate time to be taken by all performances in all classes.
+    Calculate time to be taken by all performances in all the classes in a festival event.
+
+    Parameters
+    ----------
+    classes: All the classes in a festival event.
+
+    Returns
+    -------
+    total_time: Total time allocated to all the performances.
     """
+
     total_time: int = 0
     for performance_class in classes:
-        time_taken = performance_class.performance_duration * \
-                     performance_class.performance_set.count()
-        total_time += time_taken
 
+        no_of_performances = performance_class.performance_set.count()
+        if no_of_performances == 0:  # Dont include classes which have no registered performances.
+            continue
+
+        time_taken = performance_class.performance_duration * no_of_performances
+        total_time += time_taken
     return total_time
 
 
-def session_performance_scheduling(ordered_classes: Union[QuerySet, List[Class]], session_time: int):
-    """
-    Schedule class performances into the defined sessions in  a festival event day,
-    giving priority to classes with the least performance_duration
-    """
-
-    unscheduled_performances = []
-    session_performances = []
-
-    if ordered_classes:
-        for performance_class in ordered_classes:
-
-            if performance_class.performance_duration < session_time:
-                class_performances = performance_class.performance_set.all()
-                session_performances.extend(class_performances)
-
-                time_taken = len(class_performances) * performance_class.performance_duration
-                if session_time - time_taken >= 0:
-                    session_time -= time_taken
-                    continue
-                else:
-
-                    session_time = balance_class_scheduling(
-                        remaining_class_performances=class_performances,
-                        remaining_session_time=session_time,
-                        session_performances=session_performances,
-                        unscheduled_performances=unscheduled_performances)
-
-        return tuple(session_performances), unscheduled_performances, session_time
-    else:
-        return EmptyQuerySet
-
-
-def balance_class_scheduling(
-        remaining_class_performances: Union[QuerySet, List[Class]],
-        remaining_session_time: int,
-        session_performances: list,
-        unscheduled_performances: list):
-    """
-    Schedule the performances of whichever class doesn't fit into a session individually,
-    and placing the leftover performances of that class into the unscheduled_performances list
-    to be scheduled into the next session.
+def class_performances_scheduling(
+        class_performances: list,
+        session_time: int,
+        balance_performances: list,
+        scheduled_performances: list):
     """
 
-    for performance in remaining_class_performances:
+    Parameters
+    ----------
+    balance_performances: list
+        Performances that did not fit into a session.
+    scheduled_performances: list
+        Performances already scheduled for a session.
+    class_performances: list
+        Performances to be scheduled.
+    session_time: int
+        Session time available in minutes.
 
-        if performance.performance_class.performance_duration <= remaining_session_time:
-            session_performances.append(performance)
-            remaining_session_time -= performance.performance_class.performance_duration
+    Returns
+    -------
+    """
+
+    for performance in class_performances:
+        if performance.performance_class.performance_duration <= session_time:
+            scheduled_performances.append(performance)
+            session_time -= performance.performance_class.performance_duration
         else:
-            unscheduled_performances.append(performance)
-    return remaining_session_time
+            if performance.performance_class.performance_duration > session_time:
+                balance_performances.append(performance)
+
+    return scheduled_performances, balance_performances, session_time
 
 
 def sessions_in_a_day() -> tuple:
     """
     Define the sessions allowed in a festival event day.
+
+    Returns
+    -------
+    tuple
+        A tuple containing time taken in a session and
+        the session start time object.
     """
+
     first_session_start: str = '0800'
     first_session_end: str = '1100'
     second_session_start: str = '1130'
@@ -148,17 +285,33 @@ def sessions_in_a_day() -> tuple:
     third_session_start: str = '1400'
     third_session_end: str = '1700'
 
-    first_session = get_session_duration(a=first_session_start, b=first_session_end)
-    second_session = get_session_duration(a=second_session_start, b=second_session_end)
-    third_session = get_session_duration(a=third_session_start, b=third_session_end)
+    first_session = get_session(
+        a=first_session_start, b=first_session_end)
+    second_session = get_session(
+        a=second_session_start, b=second_session_end)
+    third_session = get_session(
+        a=third_session_start, b=third_session_end)
 
-    return (
-        (first_session.total_seconds() / 60, first_session_start),
-        (second_session.total_seconds() / 60, second_session_start),
-        (third_session.total_seconds() / 60, second_session_start)
-    )
+    return first_session, second_session, third_session
 
 
-def get_session_duration(a: str, b: str):
-    """Find the time taken to complete each session"""
-    return datetime.datetime.strptime(b, "%H%M") - datetime.datetime.strptime(a, "%H%M")
+def get_session(a: str, b: str):
+    """
+    Find the time taken to complete each session.
+
+    Parameters
+    ----------
+    a : str
+        Start time.
+    b : str
+        Stop time.
+
+    Returns
+    -------
+    timedelta :  object
+        Diff between stop time and start time.
+    start time object
+    """
+    timedelta = datetime.datetime.strptime(b, "%H%M") - datetime.datetime.strptime(a, "%H%M")
+    timedelta = timedelta.seconds / 60  # Convert to minutes
+    return timedelta, datetime.datetime.strptime(a, "%H%M")
--- performances/tests/test_performance_scheduling.py
+++ performances/tests/test_performance_scheduling.py
@@ -1,70 +1,150 @@
 import datetime
+from pprint import pprint
+
 from model_bakery import baker
 from django.test import TestCase
 from performances.performance_scheduling import (
-    get_session_duration,
+    get_session,
     sessions_in_a_day,
-    balance_class_scheduling,
-    session_performance_scheduling)
+    class_performances_scheduling,
+    total_time_taken_by_all_classes,
+    divide_classes_among_theaters,
+    event_day_scheduling,
+    schedule_performances_for_each_theater)
 
 
 class PerformanceSchedulingTests(TestCase):
     def setUp(self) -> None:
-        pass
+        self.all_classes = baker.make_recipe('performances.tests.performance_class', 5)
 
-    def test_get_session_duration(self):
-        self.a = '0800'
-        self.b = '1100'
-
-        self.assertEqual(get_session_duration(a=self.a, b=self.b), datetime.timedelta(0, 10800))
-
-    def test_sessions_in_a_day(self):
-        self.assertEqual(sessions_in_a_day(), (180, 90, 180))
-
-    def test_balance_class_scheduling(self):
-        self.remaining_class = baker.make(
-            'performances.Class',
-            performance_duration=5)
-        self.remaining_class_performances = baker.make(
-            'performances.Performance',
-            performance_class=self.remaining_class,
-            _quantity=5)
-        self.unscheduled_performances = []
-        self.session_performances = []
-
-        self. balance_session = balance_class_scheduling(
-            remaining_class_performances=self.remaining_class_performances,
-            remaining_session_time=20,
-            session_performances=self.session_performances,
-            unscheduled_performances=self.unscheduled_performances
-        )
-        self.assertEqual(len(self.unscheduled_performances), 1)
-        self.assertEqual(len(self.session_performances), 4)
-        self.assertEqual(self.balance_session, 0)
-
-    def test_performance_scheduling(self):
-        self.classes = baker.make_recipe('performances.tests.performance_class', 2)
-
-        for p_class in self.classes:
+        for p_class in self.all_classes:
             performances = baker.make(
                 'performances.Performance',
                 performance_class=p_class,
                 _quantity=5)
             p_class.performance_set.set(performances)
 
-        self. assertEqual(
-            len(session_performance_scheduling(ordered_classes=self.classes, session_time=50)[0]),
-            10
-        )
+        self.performance_class = baker.make('performances.Class', performance_duration=5)
+        self.class_performances = baker.make(
+            'performances.Performance',
+            performance_class=self.performance_class, _quantity=5)
+        self.unscheduled_performances = []
+        self.session_performances = []
+
+    def test_get_session(self):
+        self.a = '0800'
+        self.b = '1100'
+
+        self.assertEqual(get_session(a=self.a, b=self.b)[0], 180)
         self.assertEqual(
-            len(session_performance_scheduling(ordered_classes=self.classes, session_time=45)[1]),
-            1
+            get_session(a=self.a, b=self.b)[1],
+            datetime.datetime.strptime('0800', "%H%M")
         )
+
+    def test_sessions_in_a_day(self):
+        self.assertEqual(sessions_in_a_day(),
+                         ((180, datetime.datetime.strptime('0800', "%H%M")),
+                          (90, datetime.datetime.strptime('1130', "%H%M")),
+                          (180, datetime.datetime.strptime('1400', "%H%M"))
+                          ))
+
+    def test_performances_scheduled_per_session(self):
         self.assertEqual(
-            session_performance_scheduling(ordered_classes=self.classes, session_time=45)[2],
-            0
-        )
+            len(class_performances_scheduling(
+                class_performances=self.class_performances, session_time=20,
+                balance_performances=[],
+                scheduled_performances=[])[0]),
+            4,
+            msg="Scheduled performances should be 4")
 
+    def test_class_performances_left_over_after_a_session(self):
+        self.assertEqual(
+            len(class_performances_scheduling(
+                class_performances=self.class_performances, session_time=20,
+                balance_performances=[],
+                scheduled_performances=[])[1]),
+            1,
+            msg="Unscheduled performances should be 1")
 
+    def test_session_time_balance_after_performances_are_scheduled(self):
+        self.assertEqual(
+            class_performances_scheduling(
+                class_performances=self.class_performances, session_time=180,
+                balance_performances=[],
+                scheduled_performances=[])[2],
+            155,
+            msg="Session time should be 155")
 
+    def test_total_time_taken_by_all_classes(self):
+        self.assertEqual(
+            total_time_taken_by_all_classes(classes=self.all_classes),
+            125)
+
+    def test_divide_classes_among_theaters(self):
+        theaters = baker.make_recipe('performances.tests.theater', _quantity=3)
+        self.total_classes = baker.make_recipe('performances.tests.performance_class', 5)
+
+        for p_class in self.total_classes:
+            performances = baker.make(
+                'performances.Performance',
+                performance_class=p_class,
+                _quantity=5)
+            p_class.performance_set.set(performances)
+
+        self.classes_among_theaters = divide_classes_among_theaters(
+            total_time_taken=125,
+            theaters=theaters,
+            classes=self.total_classes)
+
+        self.average_time_to_theater = 25
+        for key, value in self.classes_among_theaters.items():
+            self.assertLessEqual(len(value), 2, msg='Classes allocated to theater must not be more than 2')
+
+    def test_event_day_scheduling_with_no_left_over_performances(self):
+        self.assertEqual(event_day_scheduling(
+            left_over_performances=[],
+            festival_sessions=list(sessions_in_a_day()),
+            class_performances=self.class_performances,
+            performances_in_a_day_per_session={})[2],
+
+                         [[155.0, datetime.datetime(1900, 1, 1, 8, 0)],
+                          [90.0, datetime.datetime(1900, 1, 1, 11, 30)],
+                          [180.0, datetime.datetime(1900, 1, 1, 14, 0)]]
+                         )
+
+    def test_event_day_scheduling_with_left_over_performances(self):
+        self.performance_class = baker.make('performances.Class', performance_duration=40)
+        self.class_performances = baker.make(
+            'performances.Performance',
+            performance_class=self.performance_class, _quantity=5)
+        self.assertEqual(event_day_scheduling(
+            left_over_performances=[],
+            festival_sessions=list(sessions_in_a_day()),
+            class_performances=self.class_performances,
+            performances_in_a_day_per_session={})[2],
+
+                         [[20.0, datetime.datetime(1900, 1, 1, 8, 0)],
+                          [50.0, datetime.datetime(1900, 1, 1, 11, 30)],
+                          [180.0, datetime.datetime(1900, 1, 1, 14, 0)]]
+                         )
+
+    def test_schedule_performances_for_each_theater(self):
+        self.event = baker.make_recipe('performances.tests.event')
+        if self.event:
+            self.theaters = baker.make_recipe(
+                'performances.tests.theater', _quantity=3, venue=self.event)
+
+        self.total_classes = baker.make_recipe('performances.tests.performance_class', 3)
+        for p_class in self.total_classes:
+            performances = baker.make(
+                'performances.Performance',
+                performance_class=p_class,
+                _quantity=5)
+            p_class.performance_set.set(performances)
+
+        self.assertEqual(len(schedule_performances_for_each_theater(
+            event=self.event,
+            all_classes=self.total_classes)[self.event.theater_set.get(name='Chapel')]
+                             [1][datetime.datetime(1900, 1, 1, 8, 0)]),
+                         5)
 
