b4145521bc11b17677a180a61994845660ae9e3b
jack <you@example.com>
2021-05-31 04:55:26 +0300

    Add 'schedule-performances url.

--- performances/urls.py
+++ performances/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path, include
+
 from rest_framework.routers import DefaultRouter
-from .views import (RegisterInstitutions)
+
+from .views import (
+    RegisterInstitutions,
+    schedule_performances)
 
 app_name = 'performances'
 urlpatterns = [
     path('register-institution/<int:institution_pk>',
          RegisterInstitutions.as_view(),
          name='register-institution'),
+    path('schedule-performances/<str:event>',
+         schedule_performances, name='schedule-performances')
 ]
