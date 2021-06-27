34f7787bf7f8839b2a67c011e2fe441764af4a17
jack <you@example.com>
2021-06-27 06:54:56 +0300

    Change HelloWorld.vue to HomePage.vue.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <home-page msg="The Kenya Music Festival"/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomePage from './components/HomePage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomePage
   }
 }
 </script>
