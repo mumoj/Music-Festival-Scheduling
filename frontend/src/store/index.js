609a3fc3fcb989672a6ac835e1d6b54539574a6f
jack <you@example.com>
2021-07-03 14:11:39 +0300

    Add a vuex store.

---
+++ frontend/src/store/index.js
@@ -0,0 +1,34 @@
+import Vuex from 'vuex'
+import axios from 'axios'
+
+
+export default new Vuex.Store ({
+  state: {
+    loggingIn: false,
+    loginError: null,
+    loginSuccessful: false
+  },
+  mutations: {
+    loginStart: state => state.loggingIn = true,
+    loginStop: (state, errorMessage) => {
+      state.loggingIn = false;
+      state.loginError = errorMessage;
+      state.loginSuccessful = !errorMessage;
+    }
+  },
+  actions: {
+    doLogin({ commit }, loginData) {
+      commit('loginStart');
+
+      axios.post('https://reqres.in/api/login', {
+        ...loginData
+      })
+      .then(() => {
+        commit('loginStop', null)
+      })
+      .catch(error => {
+        commit('loginStop', error.response.data.error)
+      })
+    }
+  },
+})
