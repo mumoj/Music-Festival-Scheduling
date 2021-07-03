2d905a7a0dcf9cdb6a95246ba6e217910a7d4a9f
jack <you@example.com>
2021-07-03 14:10:04 +0300

    Replace bootstap-vue with regular bootstrap.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,7 @@
-import Vue from 'vue'
 import { createApp } from 'vue'
 
-import App from './App.vue'
-
-import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from './store'
 
-Vue.use(BootstrapVue)
-Vue.use(IconsPlugin)
-
+import App from './App.vue'
 
 createApp(App).use(store).mount('#app')
