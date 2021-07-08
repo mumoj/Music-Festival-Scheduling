97e62d0e3a0c16bf022f984440fe44e0047c38c3
jack <you@example.com>
2021-07-08 18:00:05 +0300

    Modularize vuex store.

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,38 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import axios from 'axios'
+
+import accounts from './modules/accounts'
+
 
 Vue.use(Vuex)
-Vue.use(axios)
+
 
 export default new Vuex.Store ({
-  state: {
-    loggingIn: false,
-    loginError: null,
-    loginSuccessful: false
-  },
-  mutations: {
-    loginStart: state => state.loggingIn = true,
-    loginStop: (state, errorMessage) => {
-      state.loggingIn = false;
-      state.loginError = errorMessage;
-      state.loginSuccessful = !errorMessage;
-    }
-  },
-  actions: {
-    doLogin({ commit }, loginData) {
-      commit('loginStart');
+    modules : {
+        accounts,
 
-      axios.post('http://127.0.0.1:8000/accounts/login/', {
-        ...loginData
-      })
-      .then((response) => {
-        console.log(response)
-        commit('loginStop', null)
-      })
-      .catch(error => {
-        commit('loginStop', error.response.data.error)
-      })
     }
-  },
 })
